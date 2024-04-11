ac471cd8ed7631c853ded5d6c04940f3999c9bf1
Daniel Tlach <daniel@tlach.cz>
2024-04-11 20:04:18 +0200

    Adds implementation

--- README.md
+++ README.md
@@ -5,8 +5,20 @@ and have for any reason can't use a password manager.
 ## Requirements
 * Go 1.22+
 
-## Defaults
-* 16 characters
-
 ## Options
 - `--length` - set the length of the password
+- `--number` - number of passwords to generate
+- `--upper` - include uppercase letters
+- `--lower` - include lowercase letters
+- `--digits` - include digits
+- `--special` - include special characters
+
+## Defaults
+* `--length` is `16` characters
+* `--number` is `1`
+* `--upper` is `true`
+* `--lower` is `true`
+* `--digits` is `true`
+* `--special` is `true`
+
+So, if all you're looking for is somewhat secure password, you can just run the program as is, no options required.
---
+++ cmd/password-generator/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+const (
+	defaultLength            = 16
+	defaultIncludeUpper      = true
+	defaultIncludeLower      = true
+	defaultIncludeDigits     = true
+	defaultIncludeSpecial    = true
+	defaultNumberOfPasswords = 1
+)
+
+var (
+	lowerLetters      = "abcdefghijklmnopqrstuvwxyz"
+	upperLetters      = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	digits            = "0123456789"
+	specialCharacters = "!@#$%^&*()-_+=~:;?<>,./|\\{}[]`"
+	length            int
+	includeUpper      bool
+	includeLower      bool
+	includeDigits     bool
+	includeSpecial    bool
+	numberOfPasswords int
+)
+
+func init() {
+	flag.IntVar(&length, "length", defaultLength, "Length of the generated password")
+	flag.BoolVar(&includeUpper, "upper", defaultIncludeUpper, "Include uppercase letters")
+	flag.BoolVar(&includeLower, "lower", defaultIncludeLower, "Include lowercase letters")
+	flag.BoolVar(&includeDigits, "digits", defaultIncludeDigits, "Include digits")
+	flag.BoolVar(&includeSpecial, "special", defaultIncludeSpecial, "Include special characters")
+	flag.IntVar(&numberOfPasswords, "number", defaultNumberOfPasswords, "Number of passwords to generate")
+	flag.Parse()
+}
+
+func containsRequiredCharacters(password string, list string) bool {
+	for _, char := range password {
+		if strings.ContainsRune(list, char) {
+			return true
+		}
+	}
+	return false
+}
+
+func containsUppercaseCharacter(password string) bool {
+	if !includeUpper {
+		return true
+	}
+
+	return containsRequiredCharacters(password, upperLetters)
+}
+func containsLowercaseCharacter(password string) bool {
+	if !includeLower {
+		return true
+	}
+
+	return containsRequiredCharacters(password, lowerLetters)
+}
+
+func containsDigits(password string) bool {
+	if !includeDigits {
+		return true
+	}
+
+	return containsRequiredCharacters(password, digits)
+}
+
+func containsSpecialCharacter(password string) bool {
+	if !includeSpecial {
+		return true
+	}
+
+	return containsRequiredCharacters(password, specialCharacters)
+}
+func generatePassword() string {
+	var passwordChars string
+	if includeUpper {
+		passwordChars += upperLetters
+	}
+	if includeLower {
+		passwordChars += lowerLetters
+	}
+	if includeDigits {
+		passwordChars += digits
+	}
+	if includeSpecial {
+		passwordChars += specialCharacters
+	}
+
+	password := make([]byte, length)
+	for i := 0; i < length; i++ {
+		password[i] = passwordChars[rand.Intn(len(passwordChars))]
+	}
+	return string(password)
+}
+
+func main() {
+	for numberOfPasswords > 0 {
+		for {
+			password := generatePassword()
+			if containsUppercaseCharacter(password) && containsLowercaseCharacter(password) && containsDigits(password) && containsSpecialCharacter(password) {
+				fmt.Println("Generated Password:", password)
+				numberOfPasswords--
+				break
+			}
+		}
+	}
+}
